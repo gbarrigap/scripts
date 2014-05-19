f74513c22de65ddd9ad8ede5a4153e865a8f5616
Guillermo Barriga Placencia <gbarrigap@yahoo.es>
2014-05-18 21:38:44 -0400

    adds code to handle big files within rescue block

--- fck.rb
+++ fck.rb
@@ -34,15 +34,55 @@
 # @see http://trollop.rubyforge.org/
 #
 
+#
+# On this machine, these are the results of some arbitrary file chunk sizes,
+# using the command $time fck check file; given a 4687385841 bytes file.
+#
+# crc32 native implementation reference results:
+#   real  1m31.887s
+#   user  0m14.148s
+#   sys 0m5.012s
+# 
+# o----------------o-----------o-----------o-----------o---------o
+# |   chunk_size   |    real   |    user   |    sys    | verbose |
+# o----------------o-----------o-----------o-----------o---------o
+# | 512            |  2m4.402s | 0m33.944s |  0m8.296s | NO
+# o----------------o-----------o-----------o-----------o---------o
+# | 1024           | 1m22.841s | 0m24.496s | 0m10.032s | NO      |
+#                    1m58.320s   0m24.860s   0m10.636s   NO
+#                    1m59.245s   0m24.376s   0m10.240s   NO
+# o----------------o-----------o-----------o-----------o---------o
+# | 1024           | 5m28.082s | 1m17.564s | 0m38.836s | YES     |
+# o----------------o-----------o-----------o-----------o---------o
+# | 1024*512       | 000000000 | 000000000 | 000000000 |
+# o----------------o-----------o-----------o-----------o---------o
+# | 1024*1024      | 1m26.112s | 0m15.084s | 0m11.148s | YES     |
+# o----------------o-----------o-----------o-----------o---------o
+# | 1024*1024      | 1m25.959s | 0m15.444s | 0m11.132s | NO      |
+#                    1m50.710s   0m15.320s   0m11.380s
+# | 1024*1024*10     1m28.748s   0m12.252s   0m11.380s
+# | 1024*1024*20     1m28.150s
+# | 1024*1024*40     1m31.976s
+# o----------------o-----------o-----------o-----------o---------o
+# | 1024*1024*100  | 1m34.911s | 0m10.188s | 0m11.580s | YES
+# o----------------o-----------o-----------o-----------o---------o
+# | 1024*1024*100  | 1m55.434s | 0m10.092s | 0m11.472s | YES     |
+# o----------------o-----------o-----------o-----------o---------o
+# | 1024*512       | 000000000 | 000000000 | 000000000 |
+# o----------------o-----------o-----------o-----------o---------o
+# 
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 require "zlib"
 require "trollop"
 
-# Init some global variables.
+# Init some global constants.
 DEBUG = false
 CRC32_REGEX = /[0-9A-Fa-f]{8}/
 DEFAULT_DELIMITER = "_"
+DEFAULT_FILE_CHUNK_SIZE = 1024*1024*30 #1024*1024*100 # 10MB
+DEFAULT_PROGRESS_STEP = 20 # BLA BLAH...
 
 #
 # Extensions to the *String* class,
@@ -64,8 +104,71 @@ class String
 
 end
 
-def crc32(filename)
+#
+# Extensions to the *File* class,
+# to allow easy file reading by chunks of data.
+#
+class File
+
+  def each_chunk(chunk_size = DEFAULT_FILE_CHUNK_SIZE)
+    yield self.read(chunk_size) until self.eof?
+  end
+
+  def each_chunk_partial(chunk_size = DEFAULT_FILE_CHUNK_SIZE)
+    yield self.read(chunk_size) until self.eof?
+  end
+
+end
+
+def crc32(filename, chunk_size = DEFAULT_FILE_CHUNK_SIZE)
+#  file_size = File.size(filename)
+#  total_hashes = file_size / chunk_size
+#  total_hashes += 1 if (file_size / chunk_size) # blah blah...
+#
+#  #total_progress_steps = total_hashes / DEFAULT_PROGRESS_STEP
+#  #total_progress_steps += 1 if (total_hashes / DEFAULT_PROGRESS_STEP) # blah, blah...
+#
+#  if (DEBUG)
+#    ARGV.clear
+#    puts "file_size: #{file_size} / chunk_size: #{chunk_size} / total_hashes: #{total_hashes}"
+#    puts "Hit enter to continue..."
+#    gets
+#  end
+#
+#  show_progress = true
+#
+#  combined_hash = 0;
+#  open(filename, "rb") do |f|
+#    remaining_hashes = total_hashes
+#    f.each_chunk_partial(chunk_size) do |chunk|
+#      combined_hash = Zlib.crc32(chunk, combined_hash)
+#
+#      if (show_progress)
+#        remaining_hashes -= 1
+#        puts "(#{remaining_hashes}/#{total_hashes}) [#{combined_hash}]"
+#      end
+#    end
+#  end
+#
+#  combined_hash = combined_hash.to_s(16).rjust(8, "0").upcase
+#
+#  puts "combined_hash: #{combined_hash}" if DEBUG
+#  
+#  combined_hash
   Zlib.crc32(open(filename).read()).to_s(16).rjust(8, "0").upcase
+
+rescue => detail
+  puts "File too big; trying to calculate hash in chunks..."
+
+  combined_hash = 0;
+  open(filename, "rb") do |f|
+    remaining_hashes = total_hashes
+    f.each_chunk_partial(chunk_size) do |chunk|
+      combined_hash = Zlib.crc32(chunk, combined_hash)
+    end
+  end
+
+  combined_hash.to_s(16).rjust(8, "0").upcase
 end
 
 def get_hashed_filename(filename, hash, delimiter)
@@ -153,11 +256,13 @@ cmd_opts = case cmd
       opt :delimiter, "Character to separate the hash from the filename", {:default => DEFAULT_DELIMITER}
       opt :dry_run, "Don't actually do anything.", :short => "-n"
       opt :quiet, "Run without showing progress."
+      opt :chunk_size, "Process file by chunks of this size (bytes)", { :default => DEFAULT_FILE_CHUNK_SIZE }
     end
   when "check" # parse check options
     Trollop::options do
       opt :verbose, "Print extra information."
       opt :force, "Check files with no apparent hash present"
+      opt :chunk_size, "Process file by chunks of this size (bytes)", { :default => DEFAULT_FILE_CHUNK_SIZE }
     end
   else
     Trollop::die "unknown subcommand #{cmd.inspect}"
